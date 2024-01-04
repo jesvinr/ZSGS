972cd736298a098dc0a436074ee2832de757bf36
jesvinr <148838062+jesvinr@users.noreply.github.com>
2024-01-04 14:57:13 +0530

    Delete SimplebankSystem/src directory

--- SimplebankSystem/src/Withdraw.java
+++
@@ -1,8 +0,0 @@
-public class Withdraw {
-    void AmountWithdraw(String AcNum, int money){
-        AllStorageModel storage = AllStorageModel.getInstance();
-        AllValidationViewModel validation = new AllValidationViewModel();
-        CreateAccount ca = storage.getAccount(AcNum);
-        ca.amount = ca.amount-money;
-    }
-}
